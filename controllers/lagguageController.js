ba388afe4edc5e4f5838571728381e6d7c8c7eb5
NWMSU-SN <S543775@nwmissouri.edu>
2021-11-12 09:58:02 -0600

    added details router

--- controllers/lagguageController.js
+++ controllers/lagguageController.js
@@ -14,8 +14,15 @@ exports.lagguage_list =async function(req, res) {
 }; 
  
 // for a specific Lagguage. 
-exports.lagguage_detail = function(req, res) { 
-    res.send('NOT IMPLEMENTED: lagguage detail: ' + req.params.id); 
+exports.lagguage_detail = async function(req, res) { 
+    console.log("detail"  + req.params.id) 
+    try { 
+        result = await Lagguage.findById( req.params.id) 
+        res.send(result) 
+    } catch (error) { 
+        res.status(500) 
+        res.send(`{"error": document for id ${req.params.id} not found`); 
+    } 
 }; 
  
 // Handle lagguage create on POST. 
